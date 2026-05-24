1b64d1f21a99b00442683ca0aea2df56f23d8c5a
Farid <mustafayevfarid2002@gmail.com>
2026-05-24 17:24:40 +0400

    https://leetcode.com/problems/3sum-closest/

---
+++ src/main/java/leetcode/Closest3Sum.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class Closest3Sum {
+    public static void main(String[] args) {
+
+        ClosestThreeSum closestThreeSum = new ClosestThreeSum();
+        System.out.println(closestThreeSum.threeSumClosest(new int[]{-1, 2, 1, -4}, 1));
+
+    }
+}
+
+class ClosestThreeSum {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+
+        int closest = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (Math.abs(sum - target) < Math.abs(closest - target)) {
+                    closest = sum;
+                }
+
+                if (sum > target) {
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    return sum;
+                }
+            }
+        }
+
+        return closest;
+    }
+}
